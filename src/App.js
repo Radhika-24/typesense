d2dbab9a251fe9116bfcf9acd54c94852a8057b9
Radhika <radhika@digitaltrons.com>
2024-04-16 12:01:23 +0530

    Typsense issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import Typesense from "typesense";
 
 function App() {
+  const typesenseClient = new Typesense.Client({
+    nodes: [
+      {
+        host: "5vaihwy8qg16stc9p.a1.typesense.net",
+        port: 443,
+        protocol: "https",
+      },
+    ],
+    apiKey: "fLCYjSOT3yTvQAXnGrI2vqFW3fRfnN3I",
+    connectionTimeoutSeconds: 5,
+  });
+
+  // const collections = await typesenseClient.collections().retrieve();
+  // console.log(collections);
+
+  useEffect(() => {
+    typesenseClient
+      .collections("dev_Creators")
+      .documents()
+      .search({
+        q: "*",
+        // facet_by: facet,
+        // filter_by: `tiktokUsername:!=NULL &&  ${allTiktokfilters}`,
+        // sort_by: sortBy,
+        // per_page: pagination.totalItems,
+        query_by: "tiktokUsername, legalName",
+        // page
+      })
+      .then((results) => {
+        console.log(results);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
